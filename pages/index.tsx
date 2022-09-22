fd265760a4d5c32152cea2fc0148a4ec2cfff761
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-09-23 00:46:40 +0900

    create topPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,30 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  Text,
+  transition,
+} from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import { supabase } from '../src/utils/supabaseClient'
 
 const Home: NextPage = () => {
   const [session, setSession]: any = useState(null)
+  const [search, setSearch]: any = useState('')
 
   useEffect(() => {
     setSession(supabase.auth.session())
-    supabase.auth.onAuthStateChange((event, session) => {
+    supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
     })
   }, [])
+
+  const handleClickSearch = () => {
+    alert('検索ボタンが押されました')
+  }
 
   return (
     <div
@@ -24,7 +38,59 @@ const Home: NextPage = () => {
         alignItems: 'center',
       }}
     >
-      {session ? <div>ログイン成功</div> : <div>ログイン失敗</div>}
+      {session ? (
+        <Box position="relative">
+          <Image src="/topImage.jpg" alt="トップイメージ" width="100%" />
+          <Box
+            p="10"
+            position="absolute"
+            top="60%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+          >
+            <Text
+              fontSize="5xl"
+              textAlign="center"
+              textColor="white"
+              textShadow="3px 3px 4px #171717"
+              mb="32"
+            >
+              あなたの声でつくる
+              <br />
+              リハビリ情報共有サイト
+              <br />
+              サービスタイトル○○○○
+            </Text>
+            <Flex>
+              <Input
+                size="lg"
+                width="80vh"
+                textColor="white"
+                type="text"
+                placeholder="リハビリ施設を入力"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button
+                onClick={handleClickSearch}
+                size="lg"
+                width="100px"
+                ml="2"
+                boxShadow="md"
+                _hover={{
+                  boxShadow: 'none',
+                  transition: '0.4s',
+                  bg: 'gray.300',
+                }}
+              >
+                検索
+              </Button>
+            </Flex>
+          </Box>
+        </Box>
+      ) : (
+        <div>ログイン失敗</div>
+      )}
     </div>
   )
 }
