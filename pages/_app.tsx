19a77c06e94fae81c6f5a64326fe90d21a9461bc
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-09-24 22:45:05 +0900

    create topSlideShow and fixed eslint, prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 
+import 'swiper/css/bundle'
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider>
