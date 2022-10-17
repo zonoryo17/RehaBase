8850c10b15d1d907b934be3aa23a59afa38b0d30
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-10-17 14:15:45 +0900

    fixed _app.tsx for deploy to vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 // pages/_app.js
 import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }:  AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
