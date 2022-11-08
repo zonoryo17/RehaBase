ebf0eb179b7fa3f45d96fe6e2250b855f263abc8
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-11-08 22:09:33 +0900

    fix for deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,86 +33,86 @@ const Home: NextPage = () => {
 
   return (
     <Box>
-      {session && (
-        <>
-          <Box position="relative">
-            <Image src="/topImage.jpg" alt="トップイメージ" width="100%" />
-            <Box
-              p="10"
-              position="absolute"
-              top="60%"
-              left="50%"
-              transform="translate(-50%, -50%)"
+      {/* {session && ( */}
+      <>
+        <Box position="relative">
+          <Image src="/topImage.jpg" alt="トップイメージ" width="100%" />
+          <Box
+            p="10"
+            position="absolute"
+            top="60%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+          >
+            <Text
+              fontSize="5xl"
+              textAlign="center"
+              textColor="white"
+              textShadow="3px 3px 4px #171717"
+              mb="32"
             >
-              <Text
-                fontSize="5xl"
-                textAlign="center"
-                textColor="white"
-                textShadow="3px 3px 4px #171717"
-                mb="32"
-              >
-                あなたの声でつくる
-                <br />
-                リハビリ情報共有サイト
-                <br />
-                RehaBase
-              </Text>
-              <Flex>
-                <Input
-                  size="lg"
-                  width="80vh"
-                  textColor="white"
-                  type="text"
-                  placeholder="リハビリ施設を入力"
-                  value={search}
-                  onChange={handleChange}
-                />
-                <Button
-                  onClick={handleClickSearch}
-                  size="lg"
-                  width="100px"
-                  ml="2"
-                  boxShadow="md"
-                  _hover={{
-                    boxShadow: 'none',
-                    transition: '0.4s',
-                    bg: 'gray.300',
-                  }}
-                >
-                  検索
-                </Button>
-              </Flex>
-            </Box>
-          </Box>
-          <Box mt="10" px="10">
-            <Text fontSize="3xl" fontWeight="bold" mb="5">
-              総合ランキング
+              あなたの声でつくる
+              <br />
+              リハビリ情報共有サイト
+              <br />
+              RehaBase
             </Text>
-            <Flex gap="5">
-              <TopPageRanking
-                name="大阪病院"
-                icon={'/crown1.jpg'}
-                image={'/JCHO_Osaka_Hospital.jpg'}
-                address={'大阪'}
+            <Flex>
+              <Input
+                size="lg"
+                width="80vh"
+                textColor="white"
+                type="text"
+                placeholder="リハビリ施設を入力"
+                value={search}
+                onChange={handleChange}
               />
-              <TopPageRanking
-                name="東京病院"
-                icon={'/crown2.jpg'}
-                image={'/Tokyo_Hospital.jpg'}
-                address={'東京'}
-              />
-              <TopPageRanking
-                name="福岡病院"
-                icon={'/crown3.jpg'}
-                image={'/Fukuoka_Hospital.jpg'}
-                address={'福岡'}
-              />
+              <Button
+                onClick={handleClickSearch}
+                size="lg"
+                width="100px"
+                ml="2"
+                boxShadow="md"
+                _hover={{
+                  boxShadow: 'none',
+                  transition: '0.4s',
+                  bg: 'gray.300',
+                }}
+              >
+                検索
+              </Button>
             </Flex>
           </Box>
-          <Slider />
-        </>
-      )}
-      {!session && <Text>ログイン失敗</Text>}
+        </Box>
+        <Box mt="10" px="10">
+          <Text fontSize="3xl" fontWeight="bold" mb="5">
+            総合ランキング
+          </Text>
+          <Flex gap="5">
+            <TopPageRanking
+              name="大阪病院"
+              icon={'/crown1.jpg'}
+              image={'/JCHO_Osaka_Hospital.jpg'}
+              address={'大阪'}
+            />
+            <TopPageRanking
+              name="東京病院"
+              icon={'/crown2.jpg'}
+              image={'/Tokyo_Hospital.jpg'}
+              address={'東京'}
+            />
+            <TopPageRanking
+              name="福岡病院"
+              icon={'/crown3.jpg'}
+              image={'/Fukuoka_Hospital.jpg'}
+              address={'福岡'}
+            />
+          </Flex>
+        </Box>
+        <Slider />
+      </>
+      {/* )} */}
+      {/* {!session && router.push('/login)' } */}
     </Box>
   );
 };
